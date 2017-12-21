9ad824ebe91bbc4e993cffbbe7e70fe84cedb8e1
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2017-12-21 14:41:08 +0100

    test: Uncomment - when preloaded with -r - test

--- test/index.js
+++ test/index.js
@@ -162,7 +162,6 @@ test('does not pass debug args to pino log method according to opts.map when aut
   debug('ns2')('test2')
 })
 
-/*
 test('when preloaded with -r, automatically logs all debug calls with log level debug to a default pino logger', (t) => {
   // emulate the preload environment
   var filename = module.filename
@@ -188,7 +187,6 @@ test('when preloaded with -r, automatically logs all debug calls with log level 
   module.filename = filename
   module.parent = parent
 })
-*/
 
 test('opts.skip filters out any matching namespaces', (t) => {
   var pinoDebug = require('../')
